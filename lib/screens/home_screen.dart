79d7c90def47458ae1eec406fbb24c10f6726373
HusniArr <husniarrasyid@gmail.com>
2023-05-05 17:14:36 +0700

    home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:booktickets/utils/app_styles.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({ Key? key }) : super(key: key);
@@ -20,6 +22,7 @@ class _HomeScreenState extends State<HomeScreen> {
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Column(
               children: [
+              const Gap(40),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -27,6 +30,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text('Good Morning', style: Styles.headLineStyle3),
+                    const Gap(5),
                     Text('Book Tickets', style: Styles.headLineStyle1,)
                   ],
                  ),
@@ -41,6 +45,31 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                   ),
                   )
+                ],
+              ),
+              const Gap(25),
+              Container(
+                  decoration:  BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFFF4F6FD)
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  child:Row(
+                    children: [
+                      const Icon(FluentSystemIcons.ic_fluent_search_regular, color: Color(0xFFBFC205)),
+                      Text(
+                        'Search',
+                        style: Styles.headLineStyle4,
+                      ),
+                      
+                    ],
+                )
+              ),
+              Row(
+                children: [
+                  Text('Upcoming Flights', style: Styles.headLineStyle2,),
+                  InkWell(
+                    child:Text('View All', style: Styles.textStyle.copyWith(color: Styles.primaryColor),))
                 ],
               )
             ],)
