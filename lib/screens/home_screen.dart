173f21dfcec2a8abc599eb7e76dd93bf607b4bf7
HusniArr <husniarrasyid@gmail.com>
2023-05-08 17:42:41 +0700

    hotel screen and ticket list

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,6 @@
+import 'package:booktickets/screens/hotel_screen.dart';
+import 'package:booktickets/screens/ticket_view.dart';
+import 'package:booktickets/utils/app_info_list.dart';
 import 'package:booktickets/utils/app_styles.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
@@ -5,7 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -19,60 +22,113 @@ class _HomeScreenState extends State<HomeScreen> {
       body: ListView(
         children: [
           Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              children: [
-              const Gap(40),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
                 children: [
-                  Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Good Morning', style: Styles.headLineStyle3),
-                    const Gap(5),
-                    Text('Book Tickets', style: Styles.headLineStyle1,)
-                  ],
-                 ),
-                  Container(
-                    height: 50,
-                    width: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      image: DecorationImage(
-                        fit: BoxFit.fitWidth,
-                        image: AssetImage('assets/images/travel.png')
+                  const Gap(40),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Good Morning', style: Styles.headLineStyle3),
+                          const Gap(5),
+                          Text(
+                            'Book Tickets',
+                            style: Styles.headLineStyle1,
+                          )
+                        ],
                       ),
+                      Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                              fit: BoxFit.fitWidth,
+                              image: AssetImage('assets/images/travel.png')),
+                        ),
+                      )
+                    ],
                   ),
+                  const Gap(25),
+                  Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: const Color(0xFFF4F6FD)),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 12, vertical: 12),
+                      child: Row(
+                        children: [
+                          const Icon(FluentSystemIcons.ic_fluent_search_regular,
+                              color: Color(0xFFBFC205)),
+                          Text(
+                            'Search',
+                            style: Styles.headLineStyle4,
+                          ),
+                        ],
+                      )),
+                  const Gap(40),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Upcoming Flights',
+                        style: Styles.headLineStyle2,
+                      ),
+                      InkWell(
+                          onTap: () {
+                            print('You are tapped');
+                          },
+                          child: Text(
+                            'View All',
+                            style: Styles.textStyle
+                                .copyWith(color: Styles.primaryColor),
+                          ))
+                    ],
                   )
                 ],
-              ),
-              const Gap(25),
-              Container(
-                  decoration:  BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: const Color(0xFFF4F6FD)
-                  ),
-                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-                  child:Row(
-                    children: [
-                      const Icon(FluentSystemIcons.ic_fluent_search_regular, color: Color(0xFFBFC205)),
-                      Text(
-                        'Search',
-                        style: Styles.headLineStyle4,
-                      ),
-                      
-                    ],
-                )
-              ),
-              Row(
-                children: [
-                  Text('Upcoming Flights', style: Styles.headLineStyle2,),
-                  InkWell(
-                    child:Text('View All', style: Styles.textStyle.copyWith(color: Styles.primaryColor),))
-                ],
-              )
-            ],)
+              )),
+          const Gap(15),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 20),
+            child: Row(
+                children: ticketList
+                    .map((singleTicket) => TicketView(ticket: singleTicket))
+                    .toList()),
+          ),
+          const Gap(15),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Hotels',
+                  style: Styles.headLineStyle2,
+                ),
+                InkWell(
+                    onTap: () {
+                      print('You are tapped');
+                    },
+                    child: Text(
+                      'View All',
+                      style:
+                          Styles.textStyle.copyWith(color: Styles.primaryColor),
+                    ))
+              ],
+            ),
+          ),
+          const Gap(15),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 20),
+            child: Row(
+                children: hotelList
+                    .map((singleHotel) => HotelScreen(hotel: singleHotel))
+                    .toList()),
           )
         ],
       ),
